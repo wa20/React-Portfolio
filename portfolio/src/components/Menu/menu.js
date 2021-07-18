40a1abc918b329844adf7f86ae0088b8ab107909
wael <78425538+wa20@users.noreply.github.com>
2021-07-18 11:01:39 +0100

    menu file created

---
+++ portfolio/src/components/Menu/menu.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react'
+import { Menu } from 'semantic-ui-react'
+
+export default class MenuHeader extends Component {
+  state = { activeItem: 'home' }
+
+  handleItemClick = (e, { name }) => this.setState({ activeItem: name })
+
+  render() {
+    const { activeItem } = this.state
+
+    return (
+      <Menu inverted>
+        <Menu.Item
+          name='home'
+          active={activeItem === 'home'}
+          onClick={this.handleItemClick}
+        />
+        <Menu.Item
+          name='messages'
+          active={activeItem === 'About'}
+          onClick={this.handleItemClick}
+        />
+        <Menu.Item
+          name='friends'
+          active={activeItem === 'Profile'}
+          onClick={this.handleItemClick}
+        />
+      </Menu>
+      
+    )
+  }
+}
