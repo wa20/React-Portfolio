f82c0513321bd62bcd84f7be7884c084415b4089
wael <78425538+wa20@users.noreply.github.com>
2021-07-19 16:32:21 +0100

    homepage added to app

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,6 +1,7 @@
 import {BrowserRouter as  Router} from 'react-router-dom'
 import Slidebar from "./components/Navbar/navbar.js"
 import MenuHeader from "./components/Menu/menu.js"
+import HomepageLayout from "./components/homePage/index.js"
 // import Project from "./components/Project/projects"
 
 function App() {
@@ -8,8 +9,9 @@ function App() {
     <div>
 
       <Router>
-        <MenuHeader />
-        <Slidebar />
+        <HomepageLayout />
+        {/* <MenuHeader /> */}
+        {/* <Slidebar /> */}
         {/* <Route path="/projects" component={Project} /> */}
       </Router>
     </div>
