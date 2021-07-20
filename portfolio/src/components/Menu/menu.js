fda0ac7b13dabbfb71de2cb9b39688a7b90b577f
wael <78425538+wa20@users.noreply.github.com>
2021-07-20 23:35:33 +0100

    menu file code commented out

--- portfolio/src/components/Menu/menu.js
+++ portfolio/src/components/Menu/menu.js
@@ -1,33 +1,33 @@
-import React, { Component } from 'react'
-import { Menu } from 'semantic-ui-react'
+// import React, { Component } from 'react'
+// import { Menu } from 'semantic-ui-react'
 
-export default class MenuHeader extends Component {
-  state = { activeItem: 'home' }
+// export default class MenuHeader extends Component {
+//   state = { activeItem: 'home' }
 
-  handleItemClick = (e, { name }) => this.setState({ activeItem: name })
+//   handleItemClick = (e, { name }) => this.setState({ activeItem: name })
 
-  render() {
-    const { activeItem } = this.state
+//   render() {
+//     const { activeItem } = this.state
 
-    return (
-      <Menu inverted>
-        <Menu.Item
-          name='home'
-          active={activeItem === 'home'}
-          onClick={this.handleItemClick}
-        />
-        <Menu.Item
-          name='messages'
-          active={activeItem === 'About'}
-          onClick={this.handleItemClick}
-        />
-        <Menu.Item
-          name='friends'
-          active={activeItem === 'Profile'}
-          onClick={this.handleItemClick}
-        />
-      </Menu>
+//     return (
+//       <Menu inverted>
+//         <Menu.Item
+//           name='home'
+//           active={activeItem === 'home'}
+//           onClick={this.handleItemClick}
+//         />
+//         <Menu.Item
+//           name='messages'
+//           active={activeItem === 'About'}
+//           onClick={this.handleItemClick}
+//         />
+//         <Menu.Item
+//           name='friends'
+//           active={activeItem === 'Profile'}
+//           onClick={this.handleItemClick}
+//         />
+//       </Menu>
       
-    )
-  }
-}
+//     )
+//   }
+// }
