7f30922f4e64d0e3d7389210b5a806a40c997600
wael <78425538+wa20@users.noreply.github.com>
2021-07-22 10:13:32 +0100

    project folder created

---
+++ portfolio/src/components/Projects/projects.js
@@ -0,0 +1,109 @@
+import { useState } from "react"
+
+
+
+const [projects, setProjects] = useState ([
+    {
+        id: 1,
+        project: "wrkwith-app",
+        image: "",
+        deployment: "https://wrkwith-app.herokuapp.com",
+        repo: "https://github.com/wa20/wrkwith-app",
+        detail: "",
+
+    },
+    {
+        id: 2,
+        project: "BookApp",
+        image: "",
+        deployment: "https://wa20.github.io/BookApp/",
+        repo: "https://github.com/wa20/BookApp",
+        detail: "",
+
+    },
+    {
+        id: 3,
+        project: "Node README Generator",
+        image: "",
+        deployment: "",
+        repo: "https://github.com/wa20/7-README-Generator",
+        detail: "",
+
+    },
+    {
+        id: 4,
+        project: "The Budget Tracker",
+        image: "",
+        deployment: "https://thebudget-tracker.herokuapp.com",
+        repo: "https://github.com/wa20/budget-tracker",
+        detail: "",
+
+    },
+    {
+        id: 5,
+        project: "The Fitness Tracker",
+        image: "",
+        deployment: "https://the-fitnesstracker.herokuapp.com/?id=60e43ebb51bc3a00155d11ac",
+        repo: "https://github.com/wa20/the-fitness-tracker",
+        detail: "",
+
+    },
+
+    {
+        id: 6,
+        project: "What is 'Regex'?",
+        image: "",
+        deployment: "",
+        repo: "https://github.com/wa20/regex-tutorial",
+        detail: "",
+
+    },
+
+    {
+        id: 7,
+        project: "Node Profile Generator",
+        image: "",
+        deployment: "",
+        repo: "https://github.com/wa20/8-Team-Profile-Generator",
+        detail: "",
+
+    },
+    {
+        id: 8,
+        project: "Ecom-site Backend",
+        image: "",
+        deployment: "",
+        repo: "https://github.com/wa20/11-E-Commerce-Site",
+        detail: "",
+
+    },
+    {
+        id: 9,
+        project: "Password Generator",
+        image: "",
+        deployment: "https://wa20.github.io/3-Password-Generator/",
+        repo: "https://github.com/wa20/3-Password-Generator",
+        detail: "",
+
+    },
+    {
+        id: 10,
+        project: "Timed Quiz",
+        image: "",
+        deployment: "https://wa20.github.io/4-Timed-Quiz/",
+        repo: "https://github.com/wa20/4-Timed-Quiz",
+        detail: "",
+
+    },
+    {
+        id: 11,
+        project: "Daily Planner",
+        image: "",
+        deployment: "https://github.com/wa20/5-Daily-Planner/settings/pages",
+        repo: "https://github.com/wa20/5-Daily-Planner",
+        detail: "",
+
+    },
+
+
+])
