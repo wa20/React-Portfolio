d43a614ba90796b9cf2362426cd12e8d014f4d18
wael <78425538+wa20@users.noreply.github.com>
2021-07-25 13:06:01 +0100

    routes created in app js file

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,23 +1,29 @@
-import { BrowserRouter as Router, Route} from "react-router-dom";
+import { BrowserRouter as Router, Redirect, Route} from "react-router-dom";
 import React from 'react';
-import { useState } from "react";
+// import { useState } from "react";
 import { render } from 'react-dom';
 import Portfolio from "./components/Portfolio";
 import About from "./components/About"
 import Home from "./components/Home";
+import Contact from "./components/Contact"
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
 
   return (
+    
     <Router>
-      <Route path="/about" component={About} />
-      <Route path="/portfolio" component={Portfolio} />
-      <Route path="/home" component={Home} />
-      {/* <Route path="/contact" component={Contact} /> */}
+     
+      <Route exact path="/about" component={About} />
+      <Route exact path="/portfolio" component={Portfolio} />
+      <Route exact path="/home" component={Home} />
+      <Route exact path="/contact" component={Contact} />
+      <Route exact path="/">
+        <Redirect to="/home"></Redirect>
+      </Route>
     </Router>
   );
 }
 
 export default App;
-// render(<App />, document.getElementById('root'));
+render(<App />, document.getElementById('root'));
